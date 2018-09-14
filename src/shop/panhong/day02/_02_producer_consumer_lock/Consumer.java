1a985f80c12f90edcecb3c72cce7bcbbbf5f8489
pan175 <1751265292@qq.com>
2018-09-14 20:27:30 +0800

    format source

--- src/shop/panhong/day02/_02_producer_consumer_lock/Consumer.java
+++ src/shop/panhong/day02/_02_producer_consumer_lock/Consumer.java
@@ -2,15 +2,17 @@ package shop.panhong.day02._02_producer_consumer_lock;
 
 public class Consumer implements Runnable {
 	private ShareResource resource = null;
-//创建一个构造器,并接收shareresource类型的对象 对其进行初始化 
+
+	// 创建一个构造器,并接收shareresource类型的对象 对其进行初始化
 	public Consumer(ShareResource resource) {
 		this.resource = resource;
 	}
-//消费者消费
+
+	// 消费者消费
 	public void run() {
 		for (int i = 0; i < 50; i++) {
-			
-		resource.popup();
+
+			resource.popup();
 		}
 	}
 
--- src/shop/panhong/day02/_02_producer_consumer_lock/Producer.java
+++ src/shop/panhong/day02/_02_producer_consumer_lock/Producer.java
@@ -1,19 +1,21 @@
 package shop.panhong.day02._02_producer_consumer_lock;
 
 public class Producer implements Runnable {
-private ShareResource resource  = null;
-//创建一个带shareresource类型参数的构造器. 下次new 的时候进行初始化
- public Producer(ShareResource resource){
-	 this.resource= resource;
- }
-	public void run() {
-for (int i = 0; i < 50; i++) {
-	if( i % 2 == 0){
-		resource.push("春哥", "男");
-	}else{
-		resource.push("凤姐", "女");
+	private ShareResource resource = null;
+
+	// 创建一个带shareresource类型参数的构造器. 下次new 的时候进行初始化
+	public Producer(ShareResource resource) {
+		this.resource = resource;
 	}
-}
+
+	public void run() {
+		for (int i = 0; i < 50; i++) {
+			if (i % 2 == 0) {
+				resource.push("春哥", "男");
+			} else {
+				resource.push("凤姐", "女");
+			}
+		}
 	}
 
 }
